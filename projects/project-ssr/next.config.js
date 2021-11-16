0bdf3eec1a22d7823f3175bbf679bfbfeaa0bd2f
haoran <haoran@advance.ai>
2021-11-16 16:00:02 +0800

    update experimental

--- projects/project-ssr/next.config.js
+++ projects/project-ssr/next.config.js
@@ -1,19 +1,40 @@
 /** @type {import('next').NextConfig} */
 const path = require("path");
 
+/**
+ * Add support to transpile modules inside node_modules
+ * https://github.com/vercel/next.js/issues/706
+ * 
+ * 
+ * using external dir (depends on next.js versions)
+ * 
+ * 1. hook webpack config, add external dir to your rules
+ * webpack: (config) => {
+ *   const oneOf = config.module.rules.find(({ oneOf }) => !!oneOf).oneOf;
+ *   oneOf?.forEach((rule) => {
+ *       if (
+ *           rule.test?.toString() ===
+ *           /\.(tsx|ts|js|cjs|mjs|jsx)$/.toString()
+ *       ) {
+ *           const p = path.resolve(__dirname, "../../packages");
+ *           rule.include ? rule.include.push(p) : (rule.include = [p]);
+ *       }
+ *   });
+ *   return config;
+ * }
+ *
+ * 
+ * 2. use experimental config (from next.js 10.1.2)
+ * https://github.com/vercel/next.js/pull/22867
+ * 
+ * experimental: {
+ *     externalDir: path.resolve(__dirname, "../../packages"),
+ * }
+ */
+
 module.exports = {
     reactStrictMode: true,
-    webpack: (config) => {
-        const oneOf = config.module.rules.find(({ oneOf }) => !!oneOf).oneOf;
-        oneOf?.forEach((rule) => {
-            if (
-                rule.test?.toString() ===
-                /\.(tsx|ts|js|cjs|mjs|jsx)$/.toString()
-            ) {
-                const p = path.resolve(__dirname, "../../packages");
-                rule.include ? rule.include.push(p) : (rule.include = [p]);
-            }
-        });
-        return config;
+    experimental: {
+        externalDir: path.resolve(__dirname, "../../packages"),
     },
 };
